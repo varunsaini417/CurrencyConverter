eee39af90c831ebdea440d3216d9755a81367131
varunsaini417 <varunsaini417@gmail.com>
2023-09-03 10:06:28 +0530

    added currency converter function for rupee and dollar

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.lang.*;
 public class CurrencyConverter {
-
-
     public static void main(String[] args) {
         
         String str;
@@ -18,6 +16,8 @@ public class CurrencyConverter {
         System.out.println("2: Dollar");
         System.out.println("3: Euro");
         choice = sc.nextInt();
+        System.out.println("Enter the amount");
+        amount = sc.nextLong();
         switch(choice){
             case 1:
                 Rupee_to_other(amount);
@@ -25,8 +25,34 @@ public class CurrencyConverter {
             case 2: 
                 Dollar_to_other(amount);
                 break;
+//            case 3:
+//                Euro_to_other(amount);
+//                break;
         }
-        
     }
     
+    public static void Rupee_to_other(double amt){
+        
+        System.out.println("1 Rupee = "+ 0.012 + "$");
+        
+        System.out.println(amt + " Rupee =" + (amt*0.012) + "$" );
+        System.out.println();
+        
+        System.out.println("1 Rupee = "+ 0.011 + "Euros");
+        System.out.println(amt + " Rupee =" + (amt*0.011) + "Euro" );
+    }
+    
+    public static void Dollar_to_other(double amt){
+        
+        System.out.println("1$ = "+ 82.72 + "Rupee");
+        
+        System.out.println(amt + "$ =" + (amt*82.72) + " Rupee" );
+        
+        
+        System.out.println("1$ = "+ 0.93 + "Euros");
+        System.out.println(amt + "$ =" + (amt*0.011) + " Euro" );
+    }
 }
+
+
+
